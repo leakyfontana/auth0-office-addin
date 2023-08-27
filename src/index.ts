1907ca7e792fbc634b27923a0f0f50a2e80e5a91
Xander Dyer <xdyer@confirmedapp.com>
2023-08-27 18:02:52 -0400

    base logic open dynamic popup

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-let message: string = 'Hello, World! I am the npm package: auth0-office-addin';
-console.log(message);
+/// <reference types="office-js" />
+
+export const showLoginPopup = (popupHTML: string) => {
+  const url = window.location.origin + popupHTML;
+
+  Office.context.ui.displayDialogAsync(
+    url,
+    { height: 70, width: 28 },
+    (result) => {
+      const dialog = result.value;
+
+      // Add event handler for the 'messageParent' event
+      dialog.addEventHandler(
+        Office.EventType.DialogMessageReceived,
+        async (args) => {
+          console.log(url);
+        }
+      );
+    }
+  );
+};
